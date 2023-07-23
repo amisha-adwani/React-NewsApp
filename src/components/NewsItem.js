1bf49423bf74023e3e5bda4c2e2d5463cf9cb2b5
amisha <amishaadwani0023@gmail.com>
2023-07-23 20:26:25 +0530

    fetched news from api

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -4,15 +4,15 @@ import Card from "react-bootstrap/Card";
 
 export class NewsItem extends Component {
   render() {
-    let { title, description } = this.props;
+    let { title, description,ImageUrl, url } = this.props;
     return (
       <div>
         <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Img variant="top" src={ImageUrl}/>
           <Card.Body>
             <Card.Title>{title}</Card.Title>
             <Card.Text>{description}</Card.Text>
-            <Button variant="primary">Go somewhere</Button>
+            <Button variant="primary" href={url}>Read More</Button>
           </Card.Body>
         </Card>
       </div>
