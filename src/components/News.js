b3a0deb5c34699c7d1d25fe63fff07e06aa4410a
amisha <amishaadwani0023@gmail.com>
2023-07-23 14:46:22 +0530

    structure defined

--- src/components/News.js
+++ src/components/News.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export class News extends Component {
+  async componentDidMount(){
+    let url='https://newsapi.org/v2/top-headlines?country=in&apiKey=0f85870f0b664682800893ce24fd3b56'
+    let data = await fetch(url)
+    let parsedData= await data.json()
+    console.log(parsedData)
+  }
   render() {
     return (
       <div>
-        News Component
-        <NewsItem></NewsItem>
+        <Container>
+        <h2>Top Headlines for today</h2>
+        <Row md={4}> 
+        <Col> <NewsItem title='title for the news' description='yes i am desc'></NewsItem> </Col>
+        <Col> <NewsItem title='title for the news' description='yes i am desc'></NewsItem> </Col>
+        <Col> <NewsItem title='title for the news' description='yes i am desc'></NewsItem> </Col>
+        <Col> <NewsItem title='title for the news' description='yes i am desc'></NewsItem> </Col>
+        </Row>
+
+        </Container>
       </div>
     )
   }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,13 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 export class NewsItem extends Component {
   render() {
+    let { title, description } = this.props;
     return (
       <div>
-        News Item
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>{title}</Card.Title>
+            <Card.Text>{description}</Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
       </div>
-    )
+    );
   }
 }
 
-export default NewsItem
+export default NewsItem;
