08b760eafa3312c0099afabf3f9457f09b55b0ba
amisha <amishaadwani0023@gmail.com>
2023-07-31 12:30:17 +0530

    spinner done

--- src/components/LoadingSpinner.js
+++ src/components/LoadingSpinner.js
@@ -1,14 +1,12 @@
-import React, { Component } from 'react';
-import { Spinner } from 'react-bootstrap'
+import React from "react";
+import { Spinner } from "react-bootstrap";
 
-class Loading extends Component {
-  render() {
-    return (
-      <div style={{textAlign :'center'}}>
-        <Spinner animation='border'/>
-      </div>
-    )
-  }
+function Loading() {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <Spinner animation="border" />
+    </div>
+  );
 }
 
-export default Loading
+export default Loading;
